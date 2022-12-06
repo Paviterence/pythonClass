2907002009831b1509042d6b1259f57462a8fae1
Terencepavi <pavithrabalan.py@gmail.com.com>
2022-12-06 23:30:46 +0530

    dec 6 class

---
+++ Dec6class.py
@@ -0,0 +1,44 @@
+# a=0
+# a+=2 #a=a+1
+# print(a)
+# b=2
+# b-=2 #b=b-2
+# print(b)
+#
+# c=3
+# c *=3 # c=c*3
+# print(c)
+#
+# d=8
+# d /=8 #d=d/8
+# print(d)
+#
+# modulus=10
+# modulus %=4
+# print("reminder:",modulus)
+#
+# Quotient=10
+# Quotient //=5
+# print("quotient:",Quotient)
+#
+# #logical
+#
+# pavi=43
+# tharani=53
+# sha=43
+#
+# print(pavi is sha)
+# print(sha is not pavi)
+#
+# print(pavi==sha)
+# print(sha!=pavi)
+#
+# a=1,2
+# print(1 in a)
+# print(10 in a)
+# print(10 not in a)
+
+
+i=10
+print(i>20 and i<20)
+print(i>20 or i<20)
